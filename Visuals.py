cf4e9b62589a672db4cf2803f98ceafe0b4267ef
Mustafa <mostafa.labib65@gmail.com>
2018-07-21 00:30:49 +0200

    First self-driving Logic

--- Visuals.py
+++ Visuals.py
@@ -100,7 +100,7 @@ def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
         l1_x1, l1_y1, l1_x2, l1_y2 = average_lane(final_lanes[lane1_id])
         l2_x1, l2_y1, l2_x2, l2_y2 = average_lane(final_lanes[lane2_id])
 
-        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2]
+        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2], lane1_id, lane2_id
     except Exception as e:
         print(str(e))
 
@@ -131,8 +131,10 @@ def process_img(image):
     # more info: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
     #                                     rho   theta   thresh  min length, max gap:
     lines = cv2.HoughLinesP(processed_img, 1, np.pi / 180, 180, 20, 15)
+    m1 = 0
+    m2 = 0
     try:
-        l1, l2 = draw_lanes(original_image, lines)
+        l1, l2, m1, m2 = draw_lanes(original_image, lines)
         cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0, 255, 0], 30)
         cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0, 255, 0], 30)
     except Exception as e:
@@ -148,19 +150,55 @@ def process_img(image):
     except Exception as e:
         pass
 
-    return processed_img, original_image
+    return processed_img, original_image, m1, m2
+
+
+def straight():
+    PressKey(W)
+    ReleaseKey(A)
+    ReleaseKey(D)
+
+
+def left():
+    PressKey(A)
+    ReleaseKey(W)
+    ReleaseKey(D)
+    ReleaseKey(A)
+
+
+def right():
+    PressKey(D)
+    ReleaseKey(A)
+    ReleaseKey(W)
+    ReleaseKey(D)
+
+def slow():
+    ReleaseKey(W)
+    ReleaseKey(A)
+    ReleaseKey(D)
 
 
 def main():
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
+
     last_time = time.time()
     while True:
         screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-        new_screen, original_image = process_img(screen)
+        new_screen, original_image, m1, m2 = process_img(screen)
         print('loop took {} seconds'.format(time.time() - last_time))
         last_time = time.time()
-        cv2.imshow('window', new_screen)
+        #cv2.imshow('window', new_screen)
         cv2.imshow('window2', cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
-        # cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+
+        if m1 < 0 and m2 < 0:
+            right()
+        elif m1 > 0 and m2 > 0:
+            left()
+        else:
+            straight()
+        cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
